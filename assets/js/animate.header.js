a263d64285ea691297b5fdada8edf363b7780a15
Mark Paine <mpainenz@gmail.com>
2017-04-03 11:00:13 +1200

    .

--- assets/js/animate.header.js
+++ assets/js/animate.header.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 
-  $('#logo-r, #logo-g, #logo-b').jrumble({
-  	x: 2,
-  	y: 2,
+  $('#logo-r').jrumble({
+  	x: 1,
+  	y: 0,
   	rotation: 0,
   	opacity: true,
-	opacityMin: .5,
-	speed: 200
+	opacityMin: .2,
+	speed: 50
+  });
+
+  $('#logo-g').jrumble({
+    x: 1,
+    y: 0,
+    rotation: 0,
+    opacity: true,
+  	opacityMin: .2,
+  	speed: 75
+  });
+
+    $('#logo-b').jrumble({
+    x: 0,
+    y: 1,
+    rotation: 0,
+    opacity: true,
+  	opacityMin: .2,
+  	speed: 75
   });
 
   $('#logo-r, #logo-g, #logo-b').trigger('startRumble');
