e6ffd6478c9498e7a28221dfe79ace7d9f92749c
Mark Paine <mpainenz@gmail.com>
2017-04-03 14:52:11 +1200

    Update animate.header.v2.js

--- assets/js/animate.header.v2.js
+++ assets/js/animate.header.v2.js
@@ -1,42 +1,17 @@
 $(document).ready(function() {
-    animateDiv();
-
+    randomAnimation();
 });
 
-function makeNewPosition() {
-    var h = 5;
-    var w = 5;
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-    return [nh, nw];
-}
 
-function animateDiv() {
-    var $target = $('#logo-r');
-    var newq = makeNewPosition($target.parent());
-    var oldq = $target.offset();
-    var speed = calcSpeed([oldq.top, oldq.left], newq);
-
+function randomAnimation() {
+    x = (Math.random() * 4) - 2
+    y = (Math.random() * 4) - 2
+    
     $('#logo-r').animate({
-        top: newq[0],
-        left: newq[1]
-    }, speed, function() {
-        animateDiv();
+        top: y,
+        left: x
+    }, 100, "swing" function() {
+        randomAnimation();
     });
 
 };
-
-function calcSpeed(prev, next) {
-
-    var x = Math.abs(prev[1] - next[1]);
-    var y = Math.abs(prev[0] - next[0]);
-
-    var greatest = x > y ? x : y;
-
-    var speedModifier = 0.1;
-
-    var speed = Math.ceil(greatest / speedModifier);
-
-    return speed;
-
-}
