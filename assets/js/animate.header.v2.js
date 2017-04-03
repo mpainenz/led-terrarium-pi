121da736e2ed3054ded81b730b059c76e94fa32a
Mark Paine <mpainenz@gmail.com>
2017-04-03 15:36:45 +1200

    Update animate.header.v2.js

--- assets/js/animate.header.v2.js
+++ assets/js/animate.header.v2.js
@@ -10,7 +10,7 @@ function randomMovement(target) {
     x = (Math.random() * 6) - 3;
     y = (Math.random() * 6) - 3;
     
-    target.animate({
+    target.velocity({
         top: y,
         left: x
     }, 1000, "swing", function() {
@@ -21,7 +21,7 @@ function randomMovement(target) {
 function randomOpacity(target) {
     o = (Math.random() * .5) + .5;
     
-    target.animate({
+    target.velocity({
         opacity: o
     }, 1000, "swing", function() {
         randomOpacity(target);
